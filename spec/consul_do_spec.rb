426871b1288f4b7482449a7f3ab9f087a87afa16
Jason Scholl <jason.e.scholl@gmail.com>
2015-09-02 13:46:55 -0700

    close blocks in the right places

--- spec/consul_do_spec.rb
+++ spec/consul_do_spec.rb
@@ -28,28 +28,27 @@ describe ConsulDo do
         ConsulDo.http_put('http://www.example.com:80/testing')
       end
     end
+  end
 
+  context 'without proxy' do
+    before(:each) do
+      stub_request(:any, "www.example.com")
+      ConsulDo.config.http_proxy = nil
+    end
 
-    context 'without proxy' do
-      before(:each) do
-        stub_request(:any, "www.example.com")
-        ConsulDo.config.http_proxy = nil
+    describe 'http_get' do
+      it "doesn't use the proxy" do
+        expect(Net::HTTP).not_to receive(:Proxy)
+        expect(Net::HTTP).to receive(:get_response)
+        ConsulDo.http_get('http://www.example.com:80/testing')
       end
+    end
 
-      describe 'http_get' do
-        it "doesn't use the proxy" do
-          expect(Net::HTTP).not_to receive(:Proxy)
-          expect(Net::HTTP).to receive(:get_response)
-          ConsulDo.http_get('http://www.example.com:80/testing')
-        end
-      end
-
-      describe 'http_put' do
-        it "doesn't use the proxy" do
-          expect(Net::HTTP).not_to receive(:Proxy)
-          expect_any_instance_of(Net::HTTP).to receive(:send_request)
-          ConsulDo.http_put('http://www.example.com:80/testing')
-        end
+    describe 'http_put' do
+      it "doesn't use the proxy" do
+        expect(Net::HTTP).not_to receive(:Proxy)
+        expect_any_instance_of(Net::HTTP).to receive(:send_request)
+        ConsulDo.http_put('http://www.example.com:80/testing')
       end
     end
   end
